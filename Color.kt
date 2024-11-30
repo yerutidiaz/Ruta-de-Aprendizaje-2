486750aedd43ce021d3135446d5ef72b8c71fc14
yerutidiaz <diazyeruti550@gmail.com>
2024-11-30 20:53:25 -0300

    Create Color.kt

---
+++ Color.kt
@@ -0,0 +1,13 @@
+package com.example.cupcake.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val LightPrimary = Color(0xFFD4A5A5) // Café claro
+val LightSecondary = Color(0xFF6A4F4B) // Café oscuro
+val LightBackground = Color(0xFFF5F5F5) // Fondo claro
+val LightSurface = Color(0xFFFFFFFF) // Blanco
+
+val DarkPrimary = Color(0xFFD4A5A5) // Café claro
+val DarkSecondary = Color(0xFF6A4F4B) // Café oscuro
+val DarkBackground = Color(0xFF2C2C2C) // Fondo oscuro
+val DarkSurface = Color(0xFF3A3A3A) // Superficie oscura
